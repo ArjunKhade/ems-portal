562850e883c03f108f103a7112d94bbeae526359
ArjunKhade <khadearjun@gmail.com>
2026-04-01 22:39:50 +0530

    implemented lazy loading

--- ems-frontend/src/app/app.routes.ts
+++ ems-frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./components/auth/login/login').then((m) => m.Login),
+  },
+  {
+    path: 'signup',
+    loadComponent: () => import('./components/auth/signup/signup').then((m) => m.Signup),
+  },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' },
+];
