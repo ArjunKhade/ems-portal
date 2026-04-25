b50583851734c048c68fa3dccf380483576eb17e
ArjunKhade <khadearjun@gmail.com>
2026-04-25 22:56:33 +0530

    updated employee model

--- ems-frontend/src/app/components/employee/employee.model.ts
+++ ems-frontend/src/app/components/employee/employee.model.ts
@@ -1,7 +1,13 @@
-
-export interface Employee {
-  id?: number;       
-  name: string;
-  location: string;
-  department: string; 
+export class Employee {
+  id?: number = 0;
+  name: string = '';
+  email: string = '';
+  department: string = '';
+  location: string = '';
+  position: string = '';
+  mobileNo: string = '';
+  dob: string = '';          
+  joiningDate: string = '';
+  salary: number = 0;
+  age: number = 0;
 }
