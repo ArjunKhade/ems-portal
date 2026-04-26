d0b1a1b2ffb3a606b1b64d3bc9fbb341e665acf5
ArjunKhade <khadearjun@gmail.com>
2026-04-26 18:48:43 +0530

    create class user entity

---
+++ ems-backend/src/main/java/com/app/ems/entities/User.java
@@ -0,0 +1,47 @@
+package com.app.ems.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@Table(name = "users")
+public class User extends BaseEntity {
+
+	
+	private String name;
+	
+	@Column(length = 30, unique = true, nullable = false)
+	private String email;
+	
+	private String password;
+	
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(
+			name = "user_roles", 
+			joinColumns = @JoinColumn(name="user_id"), 
+			inverseJoinColumns = @JoinColumn(name = "role_id")
+			)
+	private Set<Role> roles  = new HashSet<>();
+	
+	private boolean active;
+}
