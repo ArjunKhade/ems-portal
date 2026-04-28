f80bbedf415aadb7eb84502502ebe94bd95f12a6
ArjunKhade <khadearjun@gmail.com>
2026-04-28 09:10:42 +0530

    created DTO auth response

---
+++ ems-backend/src/main/java/com/app/ems/dto/AuthResponse.java
@@ -0,0 +1,18 @@
+package com.app.ems.dto;
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+
+	private String token;
+	
+	private String email;
+	
+	private String name;
+	
+	private Collection<? extends GrantedAuthority> roles;
+}
