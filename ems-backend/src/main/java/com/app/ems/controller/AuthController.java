0e066ab086f6880d0655c819373ddb782b90b376
ArjunKhade <khadearjun@gmail.com>
2026-05-03 16:34:38 +0530

    implemented login and register user

--- ems-backend/src/main/java/com/app/ems/controller/AuthController.java
+++ ems-backend/src/main/java/com/app/ems/controller/AuthController.java
@@ -1,14 +1,21 @@
 package com.app.ems.controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.app.ems.dto.AuthRequest;
+
+import com.app.ems.dto.ApiResponse;
+import com.app.ems.dto.UserSignupRequest;
 import com.app.ems.dto.AuthResponse;
-import com.app.ems.service.UserService;
+import com.app.ems.dto.UserLoginRequest;
+import com.app.ems.entities.User;
+import com.app.ems.service.IUserService;
 import com.app.ems.utils.JWTUtils;
 
 import jakarta.validation.Valid;
@@ -19,21 +26,40 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
 	
-	private final AuthenticationManager authenticationManager;
 	private final JWTUtils jwtUtils;
-	private final UserService userService;
+	private final IUserService userService;
+	private final AuthenticationManager authenticationManager;
+	
+	
+	// register new user 
+	@PostMapping("/signup")
+	public ResponseEntity<?> registerUser(@Valid @RequestBody UserSignupRequest request){
+		boolean findUserByEmail = userService.findUserByEmail(request.getEmail());
+		if(!findUserByEmail) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(userService.addUser(request));
+		}else {
+			return new ResponseEntity<ApiResponse>(new ApiResponse("Duplicate User Entry Email Id Already Exist!!!!"),
+					HttpStatus.BAD_REQUEST);
+		}
+	}
 	
 	@PostMapping("/login")
-	ResponseEntity<AuthResponse> login (@Valid @RequestBody AuthRequest request){
+	public ResponseEntity<?> validateUserCreateToken(@Valid @RequestBody UserLoginRequest request) {
+	
 		try {
-			authenticationManager.authenticate(
-					new UsernamePasswordAuthenticationToken(
-							request.getEmail(), request.getPassword())
-					);
+			// authenticate the credentials
+			Authentication authenticatedDetails = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(),
+					request.getPassword()));
+			// => auth succcess
+			User user = userService.getUserByEmail(request.getEmail());
+			return ResponseEntity
+					.ok(new AuthResponse(jwtUtils.generateJwtToken(authenticatedDetails), user.getId(), user.getName()));
+		} catch (BadCredentialsException e) { // lab work : replace this by a method in global exc handler
+			// send back err resp code
+			System.out.println("err " + e);
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
 		}
-		
-		
-		
 	}
+	
 
 }
