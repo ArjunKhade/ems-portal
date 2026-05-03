46864ae2c4b2d6f29ec7df3c0e657b287aa9ae55
ArjunKhade <khadearjun@gmail.com>
2026-05-03 16:37:06 +0530

    added jwt utils class

--- ems-backend/src/main/java/com/app/ems/utils/JWTUtils.java
+++ ems-backend/src/main/java/com/app/ems/utils/JWTUtils.java
@@ -1,5 +1,67 @@
 package com.app.ems.utils;
 
-public class JWTUtils {
+import java.util.Date;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 
+import com.app.ems.security.CustomUserDetails;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class JWTUtils {
+	@Value("${SECRET_KEY}")
+    private String jwtSecret;
+
+    @Value("${EXP_TIMEOUT}")
+    private int jwtExpirationMs;
+
+    // Convert String secret to SecretKey (recommended for jjwt > 0.11.x)
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+
+    public String generateJwtToken(Authentication authentication) {
+        CustomUserDetails userPrincipal = (CustomUserDetails) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject(userPrincipal.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+   
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(authToken);
+            return true;
+        } catch (JwtException e) {
+            log.error("Invalid JWT: {}", e.getMessage());
+        }
+        return false;
+    }
+    
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+    
+    
 }
