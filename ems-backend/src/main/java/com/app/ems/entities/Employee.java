d80bf647accfb50fcbc3e0b2eaf2da60d53cd3df
ArjunKhade <khadearjun@gmail.com>
2026-05-08 10:24:07 +0530

    updated employee class

--- ems-backend/src/main/java/com/app/ems/entities/Employee.java
+++ ems-backend/src/main/java/com/app/ems/entities/Employee.java
@@ -1,8 +1,21 @@
 package com.app.ems.entities;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Employee extends BaseEntity {
 
+	private String name;
+	
+	private String email;
+	
+	private String phone;
+	
+	
 }
