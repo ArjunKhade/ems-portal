54331c9af4b64666d3d75652878fabc5a69a9662
ArjunKhade <khadearjun@gmail.com>
2026-05-16 08:29:12 +0530

    added @Repository annotation for roles.

--- ems-backend/src/main/java/com/app/ems/repo/RoleRepository.java
+++ ems-backend/src/main/java/com/app/ems/repo/RoleRepository.java
@@ -1,10 +1,12 @@
 package com.app.ems.repo;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.app.ems.entities.Role;
 import com.app.ems.entities.UserRole;
 
-
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 	
 	Optional<Role> findByRole(UserRole role);
