f3968747d3055e62066777ef6482cc761501deb0
ArjunKhade <khadearjun@gmail.com>
2026-05-17 12:08:59 +0530

    implemented service method getAllEmployees and save employee

--- ems-backend/src/main/java/com/app/ems/service/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/com/app/ems/service/EmployeeServiceImpl.java
@@ -3,7 +3,11 @@ package com.app.ems.service;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.app.ems.dto.EmployeeRequestDto;
+import com.app.ems.dto.EmployeeResponseDto;
 import com.app.ems.entities.Employee;
+import com.app.ems.mapper.EmployeeMapper;
 import com.app.ems.repo.EmployeeRepo;
 
 @Service
@@ -11,11 +15,45 @@ public class EmployeeServiceImpl implements IEmployeeService {
 	
 	@Autowired
 	private EmployeeRepo empRepo;
+	
+	@Autowired
+	private EmployeeMapper empMapper;
 
 	@Override
-	public List<Employee> getAllEmployees() {
+	public List<EmployeeResponseDto> getAllEmployees() {
+		
+		List<Employee> employees  = empRepo.findAll();
+		
+		return empMapper.toDtoList(employees);
+		                   	
+	}
+
+	@Override
+	public EmployeeResponseDto registerEmployee(EmployeeRequestDto request) {
+		//map dto to entity
+		Employee entity = empMapper.toEntity(request);
+		//save entity
+		Employee savedEmp = empRepo.save(entity);
+		//return dto
+		return empMapper.toDto(savedEmp);
+	}
+
+	@Override
+	public EmployeeResponseDto getEmployeeById(Long empId) {
 		// TODO Auto-generated method stub
-		return empRepo.findAll();
+		return null;
+	}
+
+	@Override
+	public EmployeeResponseDto updateEmployee(Long empId, EmployeeRequestDto request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deteteEmployee(Long empId) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
