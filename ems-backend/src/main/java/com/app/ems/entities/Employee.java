f1960b8432594e1643a9e70a187fd0a2c2a575d8
ArjunKhade <khadearjun@gmail.com>
2026-05-18 10:06:59 +0530

    updated employee base entity

--- ems-backend/src/main/java/com/app/ems/entities/Employee.java
+++ ems-backend/src/main/java/com/app/ems/entities/Employee.java
@@ -1,21 +1,39 @@
 package com.app.ems.entities;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Employee extends BaseEntity {
 
 	private String name;
-	
-	private String email;
-	
-	private String phone;
+
+    private String email;
+
+    private String phone;
+
+    private String department;
+
+    private String position;
+
+    private String location;
+
+    private LocalDate joiningDate;
+
+    private LocalDate dob;
+
+    private double salary;
+
+    private int age;
 	
 	
 }
