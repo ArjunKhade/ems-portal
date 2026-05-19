3c5068bfd542bc97d208ae963b893331f8e014ef
ArjunKhade <khadearjun@gmail.com>
2026-05-19 10:34:33 +0530

    created custome exception for employee handling

---
+++ ems-backend/src/main/java/com/app/ems/exceptions/EmployeeHandlingException.java
@@ -0,0 +1,9 @@
+package com.app.ems.exceptions;
+
+@SuppressWarnings("serial")
+public class EmployeeHandlingException extends RuntimeException {
+	public EmployeeHandlingException(String message) {
+		super(message);
+	}
+
+}
