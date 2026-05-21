46a071093e4ac554bec47d1fb1219aeb4d73b7fc
ArjunKhade <khadearjun@gmail.com>
2026-05-21 11:23:05 +0530

    added required properties for employee response dto

--- ems-backend/src/main/java/com/app/ems/dto/EmployeeResponseDto.java
+++ ems-backend/src/main/java/com/app/ems/dto/EmployeeResponseDto.java
@@ -1,5 +1,7 @@
 package com.app.ems.dto;
 
+import java.time.LocalDate;
+
 import lombok.Data;
 import lombok.experimental.SuperBuilder;
 
@@ -8,10 +10,23 @@ import lombok.experimental.SuperBuilder;
 public class EmployeeResponseDto extends BaseDto {
 	
 	private String name;
-	
-	private String email;
-	
-	private String phone;
-	
+
+    private String email;
+
+    private String phone;
+
+    private String department;
+
+    private String position;
+
+    private String location;
+
+    private LocalDate joiningDate;
+
+    private LocalDate dob;
+
+    private double salary;
+
+    private int age;
 	
 }
