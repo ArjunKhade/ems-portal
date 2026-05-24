58900bdd2fbc45a3e425f8abf4433fcf66423cbb
ArjunKhade <khadearjun@gmail.com>
2026-05-24 17:16:28 +0530

    updated get employee and delete employee api config

--- ems-frontend/src/app/config/api.constant.ts
+++ ems-frontend/src/app/config/api.constant.ts
@@ -4,8 +4,8 @@ export const API = {
     SIGNUP: BASE_URL+'/auth/signup',
     LOGIN:BASE_URL+'/auth/login',
     GET_EMPLOYEES: BASE_URL+'/api/employees',
-    GET_EMPLOYEE_BY_ID : (empId: number) => `${BASE_URL}/api/employees/${empId}`,
-    DELETE_EMPLOYEE_BY_ID: (empId: number) => `${BASE_URL}/api/employees/${empId}`,
+    GET_EMPLOYEE: (empId: number) => `${BASE_URL}/api/employees/${empId}`,
+    DELETE_EMPLOYEE: (empId: number) => `${BASE_URL}/api/employees/${empId}`,
     CREATE_EMPLOYEE: BASE_URL+'/api/employees',
     UPDATE_EMPLOYEE: BASE_URL+'/api/employees',
 } 
