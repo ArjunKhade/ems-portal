f0c35e151bceda56be2f0f9d258ba3977287440c
ArjunKhade <khadearjun@gmail.com>
2026-05-29 08:45:53 +0530

    created selector using createSelector method.

---
+++ ems-frontend/src/app/store/selectors/user.selector.ts
@@ -0,0 +1,17 @@
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { UserState } from "../state/user.state";
+
+
+export const selectUserState = createFeatureSelector<UserState>("user");
+
+
+export const selectUser = createSelector(
+    selectUserState,
+    (state) => state.user
+)
+
+
+export const selectLoading = createSelector(
+    selectUserState,
+    (state) => state.loading
+)
